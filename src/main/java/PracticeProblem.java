e2ee4662ddcebbabf9d1423290f9e53e98016dbc
lily <lily.you@student.tdsb.on.ca>
2025-03-20 23:42:04 +0000

    Completed 2.3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,24 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static void bubbleSortString(String[] strings) {
+		boolean swapped = true;
+		for(int i = 0; i < strings.length && swapped; i++){
+			swapped = false;
+			for(int j = 0; j < strings.length - 1 - i; j++){
+				if(strings[j].compareToIgnoreCase(strings[j+1]) > 0){
+					String temp = strings[j];
+					strings[j] = strings[j+1];
+					strings[j+1] = temp;
+					swapped = true;
+				}
+			}
+			if(!swapped){
+				i = strings.length;
+			}
+		}
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
